133562a1e562851615428b17f555fcc0752d5664
devesh202 <deveshzope2002@gmail.com>
2026-02-10 00:09:51 +0530

    created dragable and resizable window

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import "./app.scss"
 import Dock from './Dock'
 import Nav from './components/Nav'
+import MacWindow from './components/MacWindow'
 
 const App = () => {
   return (
     <main>
       <Nav/>
+      
       <Dock/>
+      <MacWindow> 
+        <p>Terminal content goes here...</p>
+      </MacWindow>
     </main>
   )
 }
